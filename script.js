a19940126c86093f03c26a95f317fa69cd8b9c5a
dkruegr <jamesadejola@ymail.com>
2025-10-16 17:11:31 +0100

    Updated Hero & Hidden sections

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Navbar Animation on Scroll
   window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 200) {
+    if (window.scrollY > 500) {
       navbar.classList.add('scrolled');
     } else {
       navbar.classList.remove('scrolled');
